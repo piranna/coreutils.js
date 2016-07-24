44df29f8ff05cd51fdab87f43d840baf9c4c09b9
Jesús Leganés Combarro "piranna <piranna@gmail.com>
2016-07-24 19:14:32 +0200

    Basic test for `cd` function

---
+++ test/cd.js
@@ -0,0 +1,18 @@
+const homedir     = require('os').homedir
+const strictEqual = require('assert').strictEqual
+
+const cd = require('../lib/cd')
+
+
+describe('cd', function()
+{
+  it('move to $HOME', function()
+  {
+    const oldpwd = process.cwd()
+
+    cd()
+
+    strictEqual(process.cwd(), homedir())
+    strictEqual(process.env.OLDPWD, oldpwd)
+  })
+})
